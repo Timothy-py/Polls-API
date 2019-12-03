08e7f295d938b97b63608d3c7e30e60e535b2105
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-12-03 12:39:04 +0100

    developed locally

---
+++ polls/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from . import apiviews
+from rest_framework.routers import DefaultRouter
+
+app_name = 'Polls'
+
+# urlpatterns = [
+#     path('polls/', views.polls_list, name="polls_list"),
+#     path('polls/<int:pk>', views.polls_detail, name="polls_detail"),
+# ]
+
+router = DefaultRouter()
+router.register('polls', apiviews.PollViewSet, base_name='polls')
+
+urlpatterns = [
+    # path('polls/', apiviews.PollList.as_view(), name="polls_list"),
+    # path('polls/<int:pk>', apiviews.PollDetail.as_view(), name="polls_detail"),
+    # path('choices/', apiviews.ChoiceList.as_view(), name="choice_list"),
+    path('polls/<int:pk>/choices', apiviews.ChoiceList.as_view(), name="choice_list"),
+    # path('vote/', apiviews.CreateVote.as_view(), name="create_vote"),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote', apiviews.CreateVote.as_view(), name="create_vote"),
+    path('users', apiviews.UserCreate.as_view(), name="user_create"),
+]
+
+urlpatterns += router.urls
